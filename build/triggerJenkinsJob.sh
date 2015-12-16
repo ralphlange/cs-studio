e37b74861ce56458287009d6da64fc948e73c169
Eric Berryman <berryman@frib.msu.edu>
2015-12-16 10:45:40 -0500

    rename getChangedModules.sh to triggerJenkinsJob.sh

--- build/triggerJenkinsJob.sh
+++ build/triggerJenkinsJob.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-set -ev
+set -e
+
+export token
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    token=$line
+done < "$1"
 
 folderChangeSet=(`git diff-tree --name-only --no-commit-id -r HEAD | grep '\(core\|applications\)\/[[:alpha:]]' | cut -d/ -f1,2`)
 sorted_unique=(`echo "${folderChangeSet[@]}" | tr ' ' '\n' | sort -u -r | tr '\n' ' '`)
+
 for project in "${sorted_unique[@]}"
 do
   arr=(${project//// })
-  echo "cs-studio-${arr[0]}-${arr[1]}-${GIT_BRANCH#*/}"
+  jenkinsName="cs-studio-${arr[0]}-${arr[1]}-${GIT_BRANCH#*/}"
+  curl -X POST https://openepics.ci.cloudbees.com/job/${jenkinsName}/build --data token=${token}
 done
